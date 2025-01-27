1ff1159caceb78209b01c9a15fcd922cad9d73a1
Peter Walsh <peterwalsh0701@gmail.com>
2025-01-27 09:51:57 +0000

    Updated Connection Class

---
+++ database_files/connections/connections.py
@@ -0,0 +1,43 @@
+# Class definition for the Connections class
+import pyodbc
+
+class connectcls_sql_server:
+
+    def __init__(self, driver_name , server_name, db_name,connection_username, connection_password, connection_id=None, user_id=None, connection_name=None,  connection_url=None, connection_type=None):
+        self.driver_name = driver_name
+        self.server_name = server_name
+        self.db_name = db_name
+        self.connection_username = connection_username
+        self.connection_password = connection_password
+        self.connection_id = connection_id
+        self.user_id = user_id
+        self.connection_name = connection_name
+        self.connection_url = connection_url
+        self.connection_type = connection_type
+
+        
+    
+
+
+ 
+    def __str__(self):
+        return f'Connection ID: {self.connection_id}, User ID: {self.user_id}, Connection Name: {self.connection_name}, Connection Type: {self.connection_type}, Connection URL: {self.connection_url}, Connection Port: {self.connection_port}, Connection Username: {self.connection_username}, Connection Password: {self.connection_password}'
+    
+    def connect_str(self):
+        # connection string 
+        return  f'DRIVER={{{self.driver_name}}};SERVER={self.server_name};DATABASE={self.db_name};UID={self.connection_username};PWD={self.connection_password};'
+    
+    def make_connection(self):
+        try:
+            conn = pyodbc.connect(self.connect_str())
+            print("Connection to SQL Server is successful")
+            cursor = conn.cursor()
+        except pyodbc.DatabaseError as e:
+            print(f"Database connection failed: {e}")
+        return conn, cursor
+    
+    def query(self, conn, cursor, query):
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        result = [dict(zip([column[0] for column in cursor.description], row)) for row in rows]
+        return result
--- database_files/connections/main_abstraction.py
+++ database_files/connections/main_abstraction.py
@@ -1,3 +1,21 @@
 # Main file for the abstraction layer
 # This file is responsible for the abstraction layer of the database
 
+# Here we take the paramters passed from inside the system and setup the end points for the database
+
+from connections import *
+
+test_con = connectcls_sql_server('SQL Server', '192.168.1.50', 'Test_db01', 'sa', '01-SQL-DEV-01')
+
+print(test_con.connect_str())
+
+conn, cursor = test_con.make_connection()
+
+query = "select * from testtable"
+
+result = test_con.query(conn, cursor, query)
+
+print(result)
+
+
+
